d3badcca81e04c7090107415046b15107b99d77e
Nuredin <nuredin@addissoftware.com>
2026-07-25 23:03:15 +0300

    feat: add users end point

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,63 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
+const users = [
+  {
+    id: 1,
+    name: "John Smith",
+    email: "john.smith@example.com",
+    age: 28,
+    role: "admin",
+  },
+  {
+    id: 2,
+    name: "Sarah Johnson",
+    email: "sarah.johnson@example.com",
+    age: 25,
+    role: "user",
+  },
+  {
+    id: 3,
+    name: "Michael Brown",
+    email: "michael.brown@example.com",
+    age: 32,
+    role: "user",
+  },
+  {
+    id: 4,
+    name: "Emily Davis",
+    email: "emily.davis@example.com",
+    age: 22,
+    role: "editor",
+  },
+  {
+    id: 5,
+    name: "David Wilson",
+    email: "david.wilson@example.com",
+    age: 35,
+    role: "user",
+  },
+];
+
 app.use(express.json());
 
-app.get("/health", (_req, res) => {
+app.get("/health", async (_req, res) => {
   res.status(200).json({
     status: "ok",
     message: "API is healthy",
   });
 });
+
+app.get(
+  "/users",
+  async (_request: Request, response: Response): Promise<Response> => {
+    return response.json({
+      users,
+      success: true,
+      messsage: "users fetched successfully !!!",
+    });
+  },
+);
 
 export default app;
